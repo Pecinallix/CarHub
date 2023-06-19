c98ddab6ae9fd7768f755adc6848fd6918fc214c
Icaro Pecinalli <icaropecinalli@hotmail.com>
2023-06-19 12:00:35 -0300

    Criação do Footer e Header

---
+++ components/Hero.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import Image from 'next/image';
+import CustomButton from './CustomButton';
+
+const Hero = () => {
+  const handleScroll = () => {};
+
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Encontre, reserve ou alugue um carro - de forma simples!
+        </h1>
+
+        <p className="hero__subtitle">
+          Simplifique sua experiência de aluguel de carros com nosso processo de
+          reserva sem esforço.
+        </p>
+
+        <CustomButton
+          title="Encontrar Carros"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+        </div>
+
+        <div className="hero__image-overlay" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;

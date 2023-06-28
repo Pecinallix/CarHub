19289834045c01ebe7f39945aedfe62ccc21b10c
Icaro Pecinalli <icaropecinalli@hotmail.com>
2023-06-28 14:02:53 -0300

    criando catalogo

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import Image from 'next/image';
 
-import { CustomFilter, Hero, SearchBar } from '@/components';
+import { CarCard, CustomFilter, Hero, SearchBar } from '@/components';
+import { fetchCars } from '@/utils';
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -11,16 +16,33 @@ export default function Home() {
         <div className="home__text-container">
           <h1 className="text-4xl font-extrabold">Catálogo Carros</h1>
           <p>Descubra os carros que você pode gostar</p>
+        </div>
 
-          <div className="home__filters">
-            <SearchBar />
+        <div className="home__filters">
+          <SearchBar />
 
-            <div className="home_filter-container">
-              <CustomFilter title="fuel" />
-              <CustomFilter title="year" />
-            </div>
+          <div className="home_filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">
+              Oops, Sem Resultados
+            </h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
